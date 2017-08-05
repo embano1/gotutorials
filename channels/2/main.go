f3214cfe553b325251bc5fafb3a58446a815f6c8
Michael Gasch <embano1@live.com>
2017-08-05 22:34:10 +0200

    Avoid goroutine leaks

--- channels/2/main.go
+++ channels/2/main.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 
@@ -19,26 +20,38 @@ func init() {
 func main() {
 
 	sigs := make(chan os.Signal, 1)
-	ctx, cancel := context.WithCancel(context.Background())
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
+	ctx, cancel := context.WithCancel(context.Background())
 	gen, done := generator.New(ctx)
 
 	go func() {
-		for i := range gen {
-			fmt.Println("----------------------------------------")
-			fmt.Println("main.go: got", i)
-			sleep := rand.Intn(5)
-			fmt.Println("main.go: Sleeping for", sleep, "seconds")
-			time.Sleep(time.Duration(sleep) * time.Second)
+		sig := <-sigs
+		fmt.Println("main.go: Caught", sig, "Trying graceful shutdown...")
+		cancel()
+	}()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+	LOOP:
+		for {
+			select {
+			case i := <-gen:
+				fmt.Println("----------------------------------------")
+				fmt.Println("main.go: got", i)
+				sleep := rand.Intn(5)
+				fmt.Println("main.go: Sleeping for", sleep, "seconds")
+				time.Sleep(time.Duration(sleep) * time.Second)
+			case <-ctx.Done():
+				fmt.Println("main.go: We ware told to stop")
+				wg.Done()
+				break LOOP
+			}
 		}
 	}()
 
-	sig := <-sigs
-	fmt.Println("main.go: Caught", sig, "Trying graceful shutdown...")
-	cancel()
-
 	<-done
+	wg.Wait()
 	fmt.Println("main.go: Done")
-	os.Exit(0)
 }
